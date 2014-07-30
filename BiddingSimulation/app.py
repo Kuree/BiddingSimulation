2ed5299e441d9a59911b1f4a6f0f4954f757041d
Keyi Zhang <xmlc001@gmail.com>
2014-07-30 00:28:59 -0400

    Change port and web link

--- BiddingSimulation/app.py
+++ BiddingSimulation/app.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
         (r"/about", AboutHandler),
         (r"/contact", ContactHandler)
         ], debug = True, **settings)
-    application.listen(8888)
+    application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
