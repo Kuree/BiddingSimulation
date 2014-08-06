b8721bbeaba3db2b938a492c7e6900be93e10aad
Keyi Zhang <xmlc001@gmail.com>
2014-08-06 00:20:25 -0400

    Add data handler

---
+++ BiddingSimulation/handlers/DataHandler.py
@@ -0,0 +1,18 @@
+﻿import tornado.web
+from  jinja2 import Environment, FileSystemLoader
+import json
+import os
+
+class DataHandler(tornado.web.RequestHandler):
+    def get(self):
+        arg = self.get_argument("arg")
+        output = ""
+        if(arg == "firmChance"):
+            output = json.dumps(self.firmChance)
+        elif(arg == "owner"):
+            output = json.dumps(self.owner)
+        self.write(output)
+
+    def initialize(self):
+        self.firmChance = json.load(open(r"static/data/firmChance.json", "r"))
+        self.owner = json.load(open(r"static/data/owner.json", "r"))
