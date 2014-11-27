f3822479b272bafdbdc2f5eaf4e5487116fa1c90
Kuree <xmlc001@gmail.com>
2014-11-27 16:24:20 -0500

    Update the tutorials.

---
+++ BiddingSimulation/handlers/ResourcesHandler.py
@@ -0,0 +1,14 @@
+﻿import tornado.web
+from  jinja2 import Environment, FileSystemLoader
+
+class DetailsHandler(tornado.web.RequestHandler):
+    def get(self):
+        html_output = self.template.render(title="Bucknell Bidding Simulation Tutorial", ContentName="Bucknell Bidding Simulation Tutorial")
+        self.write(html_output)
+        return
+
+    def initialize(self):
+        self.TEMPLATE_FILE = "details.html"
+        self.templateLoader = FileSystemLoader( searchpath="templates/" )
+        self.templateEnv = Environment( loader=self.templateLoader )
+        self.template = self.templateEnv.get_template(self.TEMPLATE_FILE)
