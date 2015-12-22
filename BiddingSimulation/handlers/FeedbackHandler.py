b40417b15085926634d117d3b3b9c161d83cb2c5
EC2 Default User <ec2-user@ip-172-31-43-119.us-west-2.compute.internal>
2015-12-22 02:17:44 +0000

    fix the feedback bug

---
+++ BiddingSimulation/handlers/FeedbackHandler.py
@@ -0,0 +1,14 @@
+﻿import tornado.web
+from  jinja2 import Environment, FileSystemLoader
+
+class FeedbackHandler(tornado.web.RequestHandler):
+    def get(self):
+        html_output = self.template.render(title="Feedback", ContentName="Feedback")
+        self.write(html_output)
+        return
+
+    def initialize(self):
+        self.TEMPLATE_FILE = "feedback.html"
+        self.templateLoader = FileSystemLoader( searchpath="templates/" )
+        self.templateEnv = Environment( loader=self.templateLoader )
+        self.template = self.templateEnv.get_template(self.TEMPLATE_FILE)
